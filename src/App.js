9eaed7779f7c9907904482557602ac08f2cb3db4
Holly <milstowers@gmail.com>
2021-03-14 22:17:20 +0000

    update

--- src/App.js
+++ src/App.js
@@ -31,8 +31,7 @@ function App() {
     const getFilms = async () => {
       await fetch(
         "db.json"
-        //   "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7-!1900%2C2018-!0%2C5-!0%2C10-!0-!Any-!Any-!Any-!gt100-!%7Bdownloadable%7D&t=ns&cl=all&st=adv&ob=Relevance&p=1&sa=and", {
-        //   "method": "GET",
+
 
         // }
       )
